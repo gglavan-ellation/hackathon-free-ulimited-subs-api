1aa03a14767e6ad4024ef1fa96b326e198d5384d
gglavan-ellation <gheorghe.glavan@ellation.com>
2023-02-07 14:19:47 +0200

    Add winston logger

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const morgan = require("morgan");
 
 const router = require("./router");
+const logger = require("./utils/logger");
 
 const app = express();
 
@@ -15,20 +16,22 @@ app.use(router);
 
 const server = app.listen(process.env.PORT, async () => {
   try {
+    mongoose.set("strictQuery", false);
+
     await mongoose.connect(
       `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.orq0g0n.mongodb.net/?retryWrites=true&w=majority`
     );
 
-    console.log(`Example app listening on port ${process.env.PORT}`);
+    logger.warn(`Server listening on port ${process.env.PORT}`);
   } catch (e) {
-    console.log(e);
+    logger.error(e.message);
     process.exit(1);
   }
 });
 
 const gracefulShutdown = () => {
-  server.close(() => console.log("Server shutdown."));
-  mongoose.disconnect();
+  mongoose.disconnect(() => logger.info("Database disconnect"));
+  server.close(() => logger.info("Server shutdown"));
 };
 
 process.on("SIGTERM", gracefulShutdown);
