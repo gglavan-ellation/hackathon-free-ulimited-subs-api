6ec58f5fd1d2f3b98b1b7876003995cf70ee7059
gglavan-ellation <gheorghe.glavan@ellation.com>
2023-02-07 13:48:50 +0200

    Update referral schema fields

--- src/models/referral.js
+++ src/models/referral.js
@@ -2,11 +2,11 @@ const mongoose = require("mongoose");
 
 const ReferralSchema = new mongoose.Schema({
   code: { type: String, required: true, unique: true },
-  points: { type: Number, default: 0 },
+  coins: { type: Number, default: 0 },
   referees: [
     {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "ReferralSchema",
+      ref: "Referral",
     },
   ],
 });
