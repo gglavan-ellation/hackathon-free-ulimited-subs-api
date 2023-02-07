1076af1e59dce3352c48546532677727876e9924
gglavan-ellation <gheorghe.glavan@ellation.com>
2023-02-07 21:06:24 +0200

    Add https options

--- src/server.js
+++ src/server.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 const morgan = require("morgan");
 const healthCheck = require("express-healthcheck");
 const cors = require("cors");
+const https = require("https");
+const fs = require("fs");
 
 const router = require("./router");
 const logger = require("./utils/logger");
@@ -17,27 +19,29 @@ app.use(express.json());
 app.use(morgan("dev"));
 app.use(router);
 app.use("/health", healthCheck());
-app.use(
-  "/.well-known/pki-validation/E94F136A8632D66044782868CF8F1973.txt",
-  express.static(__dirname + "/cert/cert.txt")
-);
 
-console.log(__dirname + "/cert/cert.txt");
+const httpsOptions = {
+  ca: fs.readFileSync(__dirname + "/ssl/ca_bundle.crt"),
+  key: fs.readFileSync(__dirname + "/ssl/private.key"),
+  cert: fs.readFileSync(__dirname + "/ssl/certificate.crt"),
+};
 
-const server = app.listen(process.env.PORT, async () => {
-  try {
-    mongoose.set("strictQuery", false);
+const server = https
+  .createServer(httpsOptions, app)
+  .listen(process.env.PORT, async () => {
+    try {
+      mongoose.set("strictQuery", false);
 
-    await mongoose.connect(
-      `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.orq0g0n.mongodb.net/?retryWrites=true&w=majority`
-    );
+      await mongoose.connect(
+        `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.orq0g0n.mongodb.net/?retryWrites=true&w=majority`
+      );
 
-    logger.info(`Server listening on port: ${process.env.PORT}`);
-  } catch (e) {
-    logger.error(e.message);
-    process.exit(1);
-  }
-});
+      logger.info(`Server listening on port: ${process.env.PORT}`);
+    } catch (e) {
+      logger.error(e.message);
+      process.exit(1);
+    }
+  });
 
 process.on("SIGINT", () => {
   mongoose.disconnect(() => logger.info("Database disconnect"));
